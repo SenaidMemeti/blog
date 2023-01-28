eda4a060aef7b3137ede2e60db3d930ba9afa7da
SenaidMemeti <senaiidd@hotmail.com>
2023-01-28 16:19:07 +0100

    cms blog
    cms

---
+++ components/CommentsForm.jsx
@@ -0,0 +1,162 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { submitComment } from '../services';
+
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  const handleCommentSubmission = () => {
+    setError(false);
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+    if (!name || !email || !comment) {
+      setError(true);
+      return;
+    }
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    }
+    if (storeData) {
+      window.localStorage.setItem('name', name);
+      window.localStorage.setItem('email', email);
+    } else {
+      window.localStorage.removeItem('name', name);
+      window.localStorage.removeItem('email', email);
+    }
+    submitComment(commentObj)
+      .then((res) => {
+        toast.success("Votre commentaire est bien mis en review!",{position: toast.POSITION.TOP_CENTER});
+         setShowSuccessMessage(true);
+          setTimeout(() => { 
+            setShowSuccessMessage(false);
+          }, 3000);
+        
+      })
+  }
+  
+
+ /* useEffect(() => {
+    setLocalStorage(window.localStorage);
+    const initalFormData = {
+      name: window.localStorage.getItem('name'),
+      email: window.localStorage.getItem('email'),
+      storeData: window.localStorage.getItem('name') || window.localStorage.getItem('email'),
+    };
+    setFormData(initalFormData);
+  }, []);
+
+  const onInputChange = (e) => {
+    const { target } = e;
+    if (target.type === 'checkbox') {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }));
+    }
+  };
+
+  const handlePostSubmission = () => {
+    setError(false);
+    const { name, email, comment, storeData } = formData;
+    if (!name || !email || !comment) {
+      setError(true);
+      return;
+    }
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
+
+    if (storeData) {
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+    } else {
+      localStorage.removeItem('name');
+      localStorage.removeItem('email');
+    }
+
+    submitComment(commentObj)
+      .then((res) => {
+        if (res.createComment) {
+          if (!storeData) {
+            formData.name = '';
+            formData.email = '';
+          }
+          formData.comment = '';
+          setFormData((prevState) => ({
+            ...prevState,
+            ...formData,
+          }));
+          setShowSuccessMessage(true);
+          setTimeout(() => {
+            setShowSuccessMessage(false);
+          }, 3000);
+        }
+      });
+  };
+*/
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea 
+        ref={commentEl} 
+        className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+        placeholder="Comment"
+        name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input 
+        type="text" ref={nameEl} 
+        className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
+        placeholder="Name" 
+        name="name" 
+        />
+        <input 
+        type="email" ref={emailEl}
+        className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" 
+        placeholder="Email" 
+        name="email" />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true" />
+          <label className="text-gray-500 cursor-pointer" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
+        </div>
+      </div>
+      {error && <p className="text-xs text-red-500">All fields are mandatory</p>}
+      <div className="mt-8">
+        <button 
+        type="button" 
+        onClick={handleCommentSubmission} 
+        className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
+      
+        {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default CommentsForm;
+
