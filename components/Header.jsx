2e2b10b09d562d25d160aa5124e8c270995111ee
SenaidMemeti <107645725+SenaidMemeti@users.noreply.github.com>
2023-01-30 16:43:33 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     return (
         <div className="container mx-auto px-10 mb-8">
                 <div className="border-b w-full inline-block border-blue-400 py-8">
-                    <div className="text-center block">
+                    <div className="m-0 block">
                         <Link href="/">
                             <span className='cursor-pointer font-bold text-4xl text-white'>
                             Blog Senaid
