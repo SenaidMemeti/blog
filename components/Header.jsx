cc558d5527a43397142839ef20434ebca3753deb
SenaidMemeti <107645725+SenaidMemeti@users.noreply.github.com>
2023-01-30 14:07:51 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
                         </Link>
                     </div>
                     
-                    <div className="hidden md:float-left md:contents">
+                    <div className="hidden md:float-middle md:contents">
                         {categories.map((category) =>(
                         <Link key={category.slug} href={`/category/${category.slug}`}>
 <span className='md:float-right mt-2 align-middle text-middle text-white ml-4 font-semibold cursor-pointer'>
